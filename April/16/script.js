ce5691d85bc479975d3b8b1ab9ec0173127ef9be
Danish Ameer <selfless1438@gmail.com>
2026-04-17 17:22:30 +0500

    updated

--- April/16/script.js
+++ April/16/script.js
@@ -1,9 +1,9 @@
-lucide.createIcons();
 const card = document.getElementById("card");
 function toggleMode() {
     if (card.classList.contains("light")) {
         card.classList.remove("light");
     } else {
-        card.classList.add("light");   
+        card.classList.add("light");
     }
 }
+lucide.createIcons();
